fad2059391c04a33ace08ab661b0ad43e26a1c46
Braydon Platt <braydonplatt@gmail.com>
2015-01-21 20:53:52 -0700

    Finished String

--- arrayProperty.js
+++ arrayProperty.js
@@ -2,4 +2,13 @@
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
-  //code here
+String.prototype.reverse = function() {
+	var newString = '';
+	for (var i = this.length - 1; i >= 0; i--){
+		newString = newString + this[i];
+	};
+
+	return newString;
+};
+  'Braydon'.reverse();
+
